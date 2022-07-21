9c3db68d99ec6934dae2265348e66484a1e0cbb3
BryanCachimuel <bryanloyo56@gmail.com>
2022-07-20 22:55:01 -0500

    creando la sala de chat en la parte del frontend de la aplicación

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,38 @@
 import './App.css';
 import io from 'socket.io-client'
+import { useState } from 'react'
 
 const socket = io.connect("http://localhost:3002")
 
 function App() {
+
+  const [username, setUsername] = useState("")
+  const [room, setRoom] = useState("")
+
+  const UnirseSala = () => {
+    if(username !=="" && room !==""){
+      
+    }
+  }
+
   return (
     <div className="App">
-      <h1>Bienvenido</h1>
-    
+      <h1>Bienvenido a la Sala de Chat</h1>
+      <input 
+        type="text" 
+        placeholder="Jhon..." 
+        onChange={(event) => {
+          setUsername(event.target.value);
+        }}
+      />
+      <input 
+        type="text" 
+        placeholder="Sala ID..."
+        onChange={(event) => {
+          setRoom(event.target.value);
+        }}
+      />
+      <button>Unirse a la Sala</button>
     </div>
   );
 }
