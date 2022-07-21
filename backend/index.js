6ba7388078e1c45e5d9313827439187a3ca64378
BryanCachimuel <bryanloyo56@gmail.com>
2022-07-20 22:41:08 -0500

    probando conexión del socket desde el backend asta el frontend

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
-    console.log(socket.id);
+    console.log(`Usuario conectado: ${socket.id}`);
 
     socket.on("disconnect", () => {
         console.log("Usuario Desconectado", socket.id);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
+import io from 'socket.io-client'
 
+const socket = io.connect("http://localhost:3002")
 
 function App() {
   return (
